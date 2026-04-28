16daf7ef24e64c9f4da307d7468421a6c4d773cf
notgiven688 <notgiven688@mail.com>
2026-04-28 15:06:15 +0200

    DocFX header links for Vellum

---
+++ docs/template/public/main.js
@@ -0,0 +1,14 @@
+export default {
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/notgiven688/vellum',
+      title: 'GitHub'
+    },
+    {
+      icon: 'box-seam',
+      href: 'https://www.nuget.org/packages/VellumUI',
+      title: 'NuGet'
+    }
+  ]
+}
